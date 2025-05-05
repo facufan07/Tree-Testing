b9cba598b28d89e3675a3f1cf976c49d609c4fc5
Facundo <122570343+facufan07@users.noreply.github.com>
2025-05-05 16:30:07 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/modules/home/screens/Home.tsx
@@ -0,0 +1,30 @@
+export default function Home() {
+    return(
+        <>
+            <header>
+                
+            </header>
+            <main>
+                <section>
+                    <h1>Tree Testing</h1>
+                    <p>
+                        Crea tu propio estudio de Tree Testing para validar la navegación de tu sitio o app. 
+                        Sin registro, sin complicaciones.
+                    </p>
+                    <button>Crear nuevo estudio</button>
+                </section>
+                <section>
+                    <article>
+
+                    </article>
+                    <article>
+                        
+                    </article>
+                    <article>
+                        
+                    </article>
+                </section>
+            </main>
+        </>
+    )
+}
